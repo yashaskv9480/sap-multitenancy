026663d2ad3a26ae4a6b033fa0106f05fb3c046e
Yashas K V <yashas.venkateshiah@nexontis.com>
2025-09-05 17:56:47 +0530

    Added Notify admin

--- multitenant-backend/functions/notifyAdmin.js
+++ multitenant-backend/functions/notifyAdmin.js
@@ -1,5 +1,6 @@
 async function notifyAdmin(message) {
-  const webhookUrl = "";
+  const webhookUrl =
+    "https://msggroup.webhook.office.com/webhookb2/9e2af4a9-d92d-4aba-b11a-025076003d5f@763b2760-45c5-46d3-883e-29705bba49b7/IncomingWebhook/dbc74eb7e8c2451a997fed49996baa21/dc3c2c6c-5e76-4c21-b5f2-5d3635b6a3ef/V24hz_PYnyQmMImTDPA6OSwxSdY4rradHH2_bep15IgFo1";
   try {
     const res = await fetch(webhookUrl, {
       method: "POST",
@@ -15,5 +16,3 @@ async function notifyAdmin(message) {
     console.error("Error sending webhook notification:", err);
   }
 }
-
-// export if needed elsewheremodule.exports = { notifyAdmin };
